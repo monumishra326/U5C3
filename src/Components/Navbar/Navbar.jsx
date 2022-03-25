9070d1c144b65f2c2169959fdad64bf0d7837728
DANISH AHMED <danish4eng@gmail.com>
2022-03-25 17:04:45 +0530

    new

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,16 +8,16 @@ export const Navbar = () => {
         {/* Example: 
             <button className="history"> Link to history here  </button>  */}
         {/* Home component will have `/` route and classname as `home`  */}
-        <button className="Home">
+        <button className="home">
           <Link to="/">Home</Link>
         </button>
-        <button className="Mystery">
+        <button className="mystery">
           <Link to="/Mystery">Mystery</Link>
         </button>
-        <button className="Technology">
+        <button className="technology">
           <Link to="/Technology">Technology</Link>
         </button>
-        <button className="Mythology">
+        <button className="mythology">
           <Link to="/Mythology">Mythology</Link>
         </button>
         <button className="history">
